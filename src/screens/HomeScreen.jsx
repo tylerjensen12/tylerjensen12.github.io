a05de50bee0a15bdb91774b9efeebad4870437d4
Tyler Jensen <troubletimestwo97@gmail.com>
2023-03-22 19:32:17 -0600

    added more styling to HomeScreen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const HomeScreen = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h2>Hello!</h2>
-      <h1>Tyler Jensen</h1>
-      <h3>~ Software Developer/Engineer ~</h3>
+    <div className="h-screen bg-[url('/pics/homeBackground.jpg')] flex ">
+      <div className="flex flex-col items-center bg-blue-300 w-1/3 m-auto py-10 rounded-lg">
+        <h1 className="text-4xl font-medium">Tyler Jensen</h1>
+        <h3 className="text-2xl font-normal">
+          ~ Software Developer/Engineer ~
+        </h3>
+      </div>
     </div>
   );
 };
 
-export default HomeScreen
+export default HomeScreen;
